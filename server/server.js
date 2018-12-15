a729ca2818a0c3915fe6af660da2b07704ef4420
engineersworkbook <shivamkalra2013@gmail.com>
2018-12-15 20:48:33 +0530

    added new route

--- server/server.js
+++ server/server.js
@@ -25,8 +25,8 @@ app.get('/', (req,res) => {
 });
 
 eventslist = {"events": [
-    {
-        "_id":"5bead5987c1f432c7aa11397",
+    {"_id": "5bead5987cgjh32c77878787878787871397",
+        
         "event_category": "sports",
         "event_subcategory": "cricket",
 "event_location": {
@@ -52,7 +52,7 @@ eventslist = {"events": [
             "inquiry_url": "https://www.eventshigh.com/detail/Delhi/56378e1b79e6bb3ae5ebcecbab900cf0-Artistic-Planter-Workshop?src=eh-test"
         }},
         {
-            "_id": "5bead5987cgjh32c7aa11397",
+            "_id":"5bead5987c1f432c7aa11397",
             "event_category": "singing",
             "event_subcategory": "classical",
             "event_location": {
@@ -97,6 +97,7 @@ app.delete('/user/:userId/events/:eventId', (req,res) => {
     response = {
         "event":"successfully deleted"
     }
+    console.log(req.params.userId, req.params.eventId);
     res.header(200).send(response);
     //res.header(404).send("error")
 })
@@ -111,43 +112,6 @@ app.post('/user/:userId/events', (req,res) => {
 //res.header(404).send("error")
 })
 
-app.get('./events?category=:event_category&subcategory=:subcategory&min-age=:min_age&max-age=:max-age&start-date=:start_date&last-date=:last_date&place-within=:within&lat=:lat&lng=:lng&start=:start&limit=:limit',(req,res) => {
-
-    event_category = req.params.event_category;
-    subcategory = req.params.subcategory;
-    min_age = req.params.min_age;
-    max_age = req.params.max-age;
-    start_date = req.params.start_age;
-    last_date = req.params.last_date;
-    lng = req.params.lng;
-    lat = req.params.lat;
-    within = req.params.within;
-    start = req.params.start;
-    limit = req.params.start;
-
-// start and limit signifies what
-// and yes sir I'll add if to every selection criteria later on
-    var event = new Event();
-    event.find({
-        event_category,
-        event_subcategory: subcategory,
-        event_min_age:min_age,
-        event_max_age:max_age,
-        event_start_date:start_date,
-        event_last_date:last_date,
-        event_location:{
-            longitude:lng,
-            latitude:lat,
-            within:within
-        }
-    }, (results,err) => {
-        if(!results){
-            res.send("No such event exist")
-        }
-        res.header(200).send(results);
-    }) 
-})
-
 //what is this route for??
 app.put("/user/:userId/events/:eventId",(req,res) => {
     res.header(200).send("successfully done");
@@ -155,7 +119,8 @@ app.put("/user/:userId/events/:eventId",(req,res) => {
 })
 
 app.get("/user/:userId/events", (req,res) => {
-   
+    
+    
     
     res.header(200).send(eventslist);
 });
